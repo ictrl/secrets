e6331ec7461560ec33a199ed6f2c053b2d45debd
Samrat Saurav Jaiswal <coolsaurav34@gmail.com>
2019-06-20 22:58:26 +0530

    bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrpt = require("mongoose-encryption");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const app = express();
 
 app.use(express.static('public'));
@@ -18,8 +19,6 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = process.env.SECRET;
-userSchema.plugin(encrpt, { secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -36,18 +35,20 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
     const newUser = new User({
         email: req.body.username,
-        password: req. body.password
+        password: hash
     });
-   
     newUser.save(function(err){
         if (err){
             console.log(err);
         }else{
             res.render("secrets");
         }
-    })
+    });
+    });
 });
 
 app.post("/login", function(req, res){
@@ -59,9 +60,11 @@ app.post("/login", function(req, res){
             console.log(err);
         }else{
             if(foundUser){
-                if(foundUser.password === password){
-                    res.render("secrets")
-                }    
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if(result === true){
+                    res.render("secrets");
+                    }
+                });    
             }
         }
     });
