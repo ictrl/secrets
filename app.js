2ace63d2673991e834d5f853074a730fcbf53a36
Samrat Saurav Jaiswal <coolsaurav34@gmail.com>
2019-08-16 01:04:47 +0530

    login with google work with multiple id

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ passport.serializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "/auth/google/secrets",  
+    callbackURL: "http://localhost:3000/auth/google/secrets",  
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"    
 },
   function(accessToken, refreshToken, profile, cb) {
